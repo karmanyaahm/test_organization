909a830eed7934982d134d429ffb7f8de2cb6fcd
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-12-04 21:21:59 -0500

    getting started with go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	// wordPtr := flag.String("word", "foo", "a string")
+
+	// numbPtr := flag.Int("numb", 42, "an int")
+	// boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	// fmt.Println("word:", *wordPtr)
+	// fmt.Println("numb:", *numbPtr)
+	// fmt.Println("fork:", *boolPtr)
+	// fmt.Println("svar:", svar)
+	// fmt.Println("tail:", flag.Args())
+	fmt.Println(f1())
+}
+
+func f1() string {
+
+	return "a"
+}
